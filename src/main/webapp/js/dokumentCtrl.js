8893e4e89e2361c7e235e7e5a88c6dcf656c0507
Anders Karlsen <karlsenanders@gmail.com>
2013-09-21 16:57:57 +0200

    Styling document page

--- src/main/webapp/js/dokumentCtrl.js
+++ src/main/webapp/js/dokumentCtrl.js
@@ -1,12 +1,12 @@
 angular.module('minoptimera')
     .controller('DokumentCtrl', ['$scope',
         function($scope) {
-            $scope.filenames = ["nothing"];
+            $scope.filenames = [];
             $scope.upload = function() {
                 $("#uploadbtn").click();
             };
             $scope.fileSelected = function(element) {
-                $scope.filenames.push($("#uploadbtn").val());
+                $scope.filenames.push($("#uploadbtn").val().match(/[\w_.-]*?(?=\?)|[\w_.-]*$/)[0]);
                 $scope.$apply();
             };
 
