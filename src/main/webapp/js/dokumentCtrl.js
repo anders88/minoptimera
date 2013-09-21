d5c28253a059ca91e95cef335276518d1ec73750
Anders Karlsen <karlsenanders@gmail.com>
2013-09-21 14:35:44 +0200

    Adding document

--- src/main/webapp/js/dokumentCtrl.js
+++ src/main/webapp/js/dokumentCtrl.js
@@ -1,5 +1,12 @@
 angular.module('minoptimera')
     .controller('DokumentCtrl', ['$scope',
         function($scope) {
+            $scope.upload = function() {
+                $("#uploadbtn").click();
+            }
+            $scope.fileSelected = function(element) {
+                console.log($("#uploadbtn").val());
+            }
+            $scope.chosenFile = "Ingen";
 
         }]);
