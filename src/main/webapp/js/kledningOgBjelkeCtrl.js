107d06e3d3b486488de51038c8050dae681e9e56
Ørjan Markhus Lillevik <olillevik@gmail.com>
2013-09-21 19:00:30 +0200

    Adding buttons for kledning and bjelkelag

--- src/main/webapp/js/kledningOgBjelkeCtrl.js
+++ src/main/webapp/js/kledningOgBjelkeCtrl.js
@@ -1,5 +1,27 @@
 angular.module('minoptimera').controller('KledningOgBjelkeCtrl',
 		[ '$scope', function($scope) {
-			$scope.enEllerAnnenVariabel = true;
+			$scope.kledning_profil = "";
+			$scope.kledning_behandlet = "";
+			$scope.dekke = "";
+
+            $scope.activeClass = function(model,value) {
+                return (value == model) ? "btn-primary active" : "";
+            };
+
+            $scope.setProfil = function(value) {
+                $scope.kledning_profil = value;
+            };
+            
+            $scope.setBehandlet = function(value) {
+            	$scope.kledning_behandlet = value;
+            };
+            
+            $scope.setDekke= function(value) {
+            	$scope.dekke = value;
+            };
+            
+            $scope.setBjelketype= function(value) {
+            	$scope.bjelketype = value;
+            };
 			
 		} ]);
