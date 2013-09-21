d9c3714daaa0dc97331b865c60f9ba60efff609c
Ørjan Markhus Lillevik <olillevik@gmail.com>
2013-09-21 12:46:36 +0200

    Adding Takstoler og Byggesett to kalkyle for...

--- src/main/webapp/js/kalkyleMenyCtrl.js
+++ src/main/webapp/js/kalkyleMenyCtrl.js
@@ -1,15 +1,38 @@
-angular.module('minoptimera')
-    .controller('KalkyleMenyCtrl', ['$scope',
-        function($scope) {
-            $scope.vinduTagged = false;
-            $scope.vinduicon = "img/dor-utvendig.png";
+angular.module('minoptimera').controller('KalkyleMenyCtrl',
+		[ '$scope', function($scope) {
+			$scope.takstolerTagged = false;
+			$scope.takstolerIcon = "img/takstoler.png";
 
-            $scope.toggleVinduIcon = function() {
-                $scope.vinduTagged = !$scope.vinduTagged;
-                if ($scope.vinduTagged) {
-                    $scope.vinduicon = "img/dor-utvendig-valgt.png";
-                } else {
-                    $scope.vinduicon = "img/dor-utvendig.png";
-                }
-            }
-        }]);
+			$scope.byggesettTagged = false;
+			$scope.byggesettIcon = "img/byggesett.png";
+
+			$scope.vinduTagged = false;
+			$scope.vinduIcon = "img/dor-utvendig.png";
+
+			$scope.toggleVinduIcon = function() {
+				$scope.vinduTagged = !$scope.vinduTagged;
+				if ($scope.vinduTagged) {
+					$scope.vinduIcon = "img/dor-utvendig-valgt.png";
+				} else {
+					$scope.vinduIcon = "img/dor-utvendig.png";
+				}
+			}
+
+			$scope.toggleTakstolerIcon = function() {
+				$scope.takstolerTagged = !$scope.takstolerTagged;
+				if ($scope.takstolerTagged) {
+					$scope.takstolerIcon = "img/dor-utvendig-valgt.png";
+				} else {
+					$scope.takstolerIcon = "img/takstoler.png";
+				}
+			}
+
+			$scope.toggleByggesettIcon = function() {
+				$scope.byggesettTagged = !$scope.byggesettTagged;
+				if ($scope.byggesettTagged) {
+					$scope.byggesettIcon = "img/dor-utvendig-valgt.png";
+				} else {
+					$scope.byggesettIcon = "img/byggesett.png";
+				}
+			}
+		} ]);
